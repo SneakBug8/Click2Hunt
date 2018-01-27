a3d39f6b457535891ce49d93fdfd2308f3d903b7
SneakBug8 <sneakbug8@gmail.com>
2018-01-27 16:49:01 +0300

    Quit button, UI improvements

---
+++ Assets/scripts/UI/QuitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		gameObject.GetComponent<Button>().onClick.AddListener (Quit);
+	}
+
+	void Quit() {
+		Application.Quit ();
+	}
+}
