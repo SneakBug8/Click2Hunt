e7a0ffed959a24c3ee9a845b038a659642997a73
SneakBug8 <sneakbug8@gmail.com>
2018-01-30 23:18:41 +0300

    Link to website

---
+++ Assets/scripts/UI/LinkButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LinkButton : MonoBehaviour {
+	void Start() {
+		gameObject.GetComponent<Button>().onClick.AddListener (OnClick);
+	}
+
+	void OnClick() {
+		Application.OpenURL ("https://sneakbug8.com");
+	}
+}
