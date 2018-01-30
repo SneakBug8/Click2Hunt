8ce1169a988a75b4c7d3fca23bf93c1753f7879d
SneakBug8 <sneakbug8@gmail.com>
2018-01-30 23:18:49 +0300

    SoundLibrary

---
+++ Assets/scripts/ScriptableObjects/SoundLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Configs/SoundLibrary")]
+public class SoundLibrary : ScriptableObject {
+	public AudioClip OnButtonClick;
+	public AudioClip OnShot;
+	public AudioClip OnMonsterAttack;
+	public AudioClip OnMonsterDeath;
+	public AudioClip OnBought;
+}
