81434b8195ac45d5ddaccb1225eb060ccc7e6b35
SneakBug8 <sneakbug8@gmail.com>
2018-02-15 13:20:21 +0300

    Code cleanup

--- Assets/Editor/ClearPrefs.cs
+++ Assets/Editor/ClearPrefs.cs
@@ -11,19 +11,4 @@ public class ClearPrefs : MonoBehaviour {
 		PlayerPrefs.DeleteAll ();
 		Debug.Log ("Deleted player preferences");
 	}
-
-	[MenuItem("Tools/Add money")]
-	static void AddMoney() {
-		PlayerPrefs.SetInt ("Gold", PlayerPrefs.GetInt ("Gold", 0) + 1000);
-		Debug.Log ("Added 1000 gold");
-	}
-
-	/*[MenuItem("Tools/List Materials")]
-	static void ListMaterials() {
-		int j = 0;
-		foreach (Material i in Selection.activeGameObject.GetComponent<Text>().material) {
-			print(string.Format("[{0}] {1}", j, i.name));
-			j++;
-		}
-	} */
 }
